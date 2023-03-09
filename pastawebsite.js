8ea58aa69e9ebe53b6cc19867b6989dee6151fd8
Ife Njock-Barnes <ife1998@hotmail.com>
2023-03-09 15:52:32 +0000

    fv

--- pastawebsite.js
+++ pastawebsite.js
@@ -1,14 +1,11 @@
-// Show the pop-up message
 function showPopup() {
     document.getElementById("intro-popup").style.display = "block";
   }
   
-  // Hide the pop-up message
   function closePopup() {
     document.getElementById("intro-popup").style.display = "none";
   }
   
-  // Show the pop-up message when the page is loaded
   window.onload = function() {
     showPopup();
   };
